de5fe668b7f4e4c8f4755857f54caf006640a912
xiaxing <xiaxing@le.com>
2016-06-17 15:11:00 +0800

    [UI] add camera preview function, can switch front and rear camera, but preview data can not be catch yet

--- app/src/main/java/com/shyla/main/LiveStreamingFragment.java
+++ app/src/main/java/com/shyla/main/LiveStreamingFragment.java
@@ -1,14 +1,39 @@
 package com.shyla.main;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.pm.PackageManager;
+
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.util.Size;
 import android.view.LayoutInflater;
+import android.view.Surface;
+import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.util.Arrays;
 
 import com.shyla.asmqtt.R;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,7 +43,12 @@ import com.shyla.asmqtt.R;
  * Use the {@link LiveStreamingFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class LiveStreamingFragment extends Fragment {
+@TargetApi(Build.VERSION_CODES.LOLLIPOP)
+public class LiveStreamingFragment extends Fragment implements TextureView.SurfaceTextureListener {
+    private static final String TAG = "LiveStreaming";
+
+    private static final int REQUEST_CAMERA_RESULT = 100;
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -27,6 +57,19 @@ public class LiveStreamingFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private static final String DEFAULT_CAMERA = String.valueOf(0);
+    private static final String REAR_CAMERA = DEFAULT_CAMERA;
+    private static final String FRONT_CAMERA = String.valueOf(1);
+    private String mCameraId = DEFAULT_CAMERA;
+    private CameraDevice mCameraDevice = null;
+
+    private ImageView mCameraSwitcher;
+    private TextureView mPreviewView;
+    private Handler mHandler;
+    private HandlerThread mThreadHandler;
+    private Size mPreviewSize;
+    private CaptureRequest.Builder mPreviewBuilder;
 
     private OnFragmentInteractionListener mListener;
 
@@ -65,7 +108,36 @@ public class LiveStreamingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_live_streaming, container, false);
+        View view = inflater.inflate(R.layout.fragment_live_streaming, null);
+        initLooper();
+        initUIAndListener(view);
+        return view;
+
+        // backup
+        // inflater.inflate(R.layout.fragment_live_streaming, container, false)
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        mCameraSwitcher = (ImageView) view.findViewById(R.id.btn_camera_switcher);
+        mCameraSwitcher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchCamera();
+            }
+        });
+    }
+
+
+    private void initLooper() {
+        mThreadHandler = new HandlerThread("CAMERA2");
+        mThreadHandler.start();
+        mHandler = new Handler(mThreadHandler.getLooper());
+    }
+
+    private void initUIAndListener(View v) {
+        mPreviewView = (TextureView) v.findViewById(R.id.textureview);
+        mPreviewView.setSurfaceTextureListener(this);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -81,8 +153,7 @@ public class LiveStreamingFragment extends Fragment {
         if (context instanceof OnFragmentInteractionListener) {
             mListener = (OnFragmentInteractionListener) context;
         } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+            throw new RuntimeException(context.toString() + " must implement OnFragmentInteractionListener");
         }
     }
 
@@ -90,6 +161,143 @@ public class LiveStreamingFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+        closeCamera();
+    }
+
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+        openCamera(mCameraId);
+    }
+
+    private void switchCamera() {
+        if (mCameraDevice == null)
+            mCameraId = DEFAULT_CAMERA;
+
+        mCameraId = mCameraDevice.getId();
+        mCameraDevice.close();
+        mCameraDevice = null;
+
+        if (mCameraId == REAR_CAMERA) {
+            openCamera(FRONT_CAMERA);
+        } else {
+            openCamera(REAR_CAMERA);
+        }
+    }
+
+    private void openCamera(String cameraId) {
+        CameraManager cameraManager = (CameraManager) getActivity().getSystemService(Context.CAMERA_SERVICE);
+        try {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                    CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(cameraId);
+                    StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+                    mPreviewSize = map.getOutputSizes(SurfaceTexture.class)[0];
+                    cameraManager.openCamera(cameraId, mCameraDeviceStateCallback, mHandler);
+                } else {
+                    if (shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA)) {
+                        Toast.makeText(getActivity(), "No Permission to use the Camera services", Toast.LENGTH_SHORT).show();
+                    }
+                    requestPermissions(new String[]{android.Manifest.permission.CAMERA}, REQUEST_CAMERA_RESULT);
+                }
+            } else {
+                CameraCharacteristics characteristics = cameraManager.getCameraCharacteristics(cameraId);
+                StreamConfigurationMap map = characteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+                mPreviewSize = map.getOutputSizes(SurfaceTexture.class)[0];
+                cameraManager.openCamera(cameraId, mCameraDeviceStateCallback, mHandler);
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void closeCamera() {
+        if (mCameraDevice == null)
+            return;
+
+        mCameraDevice.close();
+        mCameraDevice = null;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CAMERA_RESULT:
+                if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getActivity(), "Cannot run application because camera service permission have not been granted", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+                break;
+        }
+    }
+
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+    }
+
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        return false;
+    }
+
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+        Log.v(TAG, "onSurfaceTextureUpdated");
+    }
+
+    private CameraDevice.StateCallback mCameraDeviceStateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(CameraDevice camera) {
+            try {
+                startPreview(camera);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onDisconnected(CameraDevice camera) {
+        }
+
+        @Override
+        public void onError(CameraDevice camera, int error) {
+        }
+    };
+
+    private void startPreview(CameraDevice camera) throws CameraAccessException {
+        SurfaceTexture texture = mPreviewView.getSurfaceTexture();
+        texture.setDefaultBufferSize(mPreviewSize.getWidth(), mPreviewSize.getHeight());
+        Surface surface = new Surface(texture);
+        try {
+            mPreviewBuilder = camera.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+        mPreviewBuilder.addTarget(surface);
+        camera.createCaptureSession(Arrays.asList(surface), mSessionStateCallback, mHandler);
+
+        mCameraDevice = camera;
+    }
+
+    private CameraCaptureSession.StateCallback mSessionStateCallback = new CameraCaptureSession.StateCallback() {
+        @Override
+        public void onConfigured(CameraCaptureSession session) {
+            try {
+                updatePreview(session);
+            } catch (CameraAccessException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onConfigureFailed(CameraCaptureSession session) {
+        }
+    };
+
+    private void updatePreview(CameraCaptureSession session) throws CameraAccessException {
+        session.setRepeatingRequest(mPreviewBuilder.build(), null, mHandler);
     }
 
     /**
